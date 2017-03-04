f6ccc9e64be8e519d6bc5ed4f7b6591bed1c3fc6
Thawatchai <thawatchai.deartk@gmail.com>
2017-03-04 23:34:54 +0700

    Test function in utils

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,6 +1,12 @@
 const expect = require('chai').expect;
 const Select = require('../src/index');
 
+import {
+  getValueArray,
+  getValueString,
+  getValueSelected,
+} from '../src/utils';
+
 describe('Select', function() {
   describe('temp', function() {
     it('true should be true', function(){
@@ -8,3 +14,61 @@ describe('Select', function() {
     });
   });
 });
+
+describe('Util', function() {
+  it('getValueArray: Input array', () => {
+    const value = [
+      {
+        label: 'Man',
+        value: 'M',
+      },
+      {
+        label: 'Women',
+        value: 'W',
+      },
+    ];
+    const result = ['M', 'W'];
+    expect(getValueArray(value)).to.eql(result);
+  });
+
+  it('getValueArray: Input object', () => {
+    const value = {
+      label: 'Man',
+      value: 'M',
+    };
+    const result = ['M'];
+    expect(getValueArray(value)).to.eql(result);
+  });
+
+  it('getValueArray: Input string', () => {
+    const value = 'M,W';
+    const result = ['M', 'W'];
+    expect(getValueArray(value)).to.eql(result);
+  });
+
+  it('getValueString', () => {
+    const value = ['M', 'W'];
+    const result = 'M,W';
+    expect(getValueString(value)).to.eql(result);
+  });
+
+  it('getValueSelected', () => {
+    const options = [
+      {
+        label: 'Man',
+        value: 'M',
+      },
+      {
+        label: 'Women',
+        value: 'W',
+      },
+    ];
+    const result = {
+      label: 'Man',
+      value: 'M',
+    };
+    expect(getValueSelected(options, 'M')).to.eql(result);
+  });
+  
+});
+
